6dd22d735ffd42c0ceb6874164f2489421483cba
Brian <brnhache@gmail.com>
2019-02-28 00:06:47 -0500

    add PrimaryArithmetic.java

--- src/PrimaryArithmetic.java
+++ src/PrimaryArithmetic.java
@@ -10,14 +10,40 @@ import java.util.Scanner;
 public class PrimaryArithmetic {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int num1;
-        int num2;
-        int count;
+        int num1 = 1;
+        int num2 = 1;
 
-        do{
+
+        while(num1 != 0 && num2 != 0){
+            int count = 0;
             num1 = scan.nextInt();
             num2 = scan.nextInt();
 
-        }while(num1 != 0 && num2 != 0);
+            if (num1 == 0 && num2 == 0){
+                break;
+            }
+
+            int small = Math.min(num1, num2);
+            int big = Math.max(num1, num2);
+
+            while(small > 0){
+                int num1digit = small % 10;
+                int num2digit = big % 10;
+                small /= 10;
+                big /= 10;
+                if(num1digit + num2digit >= 10){
+                    count ++;
+                }
+
+            }
+            if (count == 0){
+                System.out.println("No carry operation.");
+            }else if(count == 1){
+                System.out.println("1 carry operation.");
+            }else{
+                System.out.println(count + " carry operations.");
+            }
+
+        }
     }
 }
